fe97f4e6c2cc01ce06f414943eaa0ed581072da6
T540P-SL5NET\Administrator <sl5softwarelab@gmail.com>
2018-03-23 20:35:24 +0100

    this commits are fully automatic produced by script automatic-git-push-in-intellij.ahk

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -1,16 +1,14 @@
 <?php
-/ ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'wp_demo');
+define('DB_NAME', 'bitnami_wordpress');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'password');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1:3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
