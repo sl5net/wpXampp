a2da0c233127f6f090ab2ea60aa2a030c459009e
T540P-SL5NET\Administrator <sl5softwarelab@gmail.com>
2018-03-23 19:49:51 +0100

    this commits are fully automatic produced by script automatic-git-push-in-intellij.ahk

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-test
+
 <?php
+phpinfo()
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
